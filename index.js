d75cb5793873240f443eefc166c1277a8601a3c3
Rolf Küffner <rolfkueffner@t-online.de>
2017-03-22 15:24:50 +0100

    working as intended

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 var package = require('./package.json');
 var _ = require('lodash');
-var MongoClient = require('mongodb').MongoClient;
-var mongo;
-
-
-MongoClient.connect('mongodb://localhost:27017/bot', function(err, db) {
-  if (err) {
-    console.log("error in mongo connect", err);
-    process.exit(0);
-  }
-  mongo=db;
-})
 
 
 var Module = function (bot) {
@@ -27,19 +16,94 @@ var Module = function (bot) {
   };
   this.commands = {};
 
-  this.commands.poll = function(channel, args, user) {
-    var splittedText = args.split("|");
-    var question = splittedText[0];
-    var response = "";
+  var number = [];
+  var answerCounter = 0;
+  var userVote = [];
+  var pollTester = 0;
+  var answers = [];
 
-    for(i=1; i<splittedText.length ; i++){
-      response += splittedText[i];
+  this.commands.poll = function(channel, args, user) {
+
+    if(pollTester === 1){
+      bot.postMessage(channel, "Already a poll in progress!");
     }
-    console.log("RESPONSE", response);
-    var realResponse = response.split("|").join("\n"," ");
-    console.log("REALRESPONSE", realResponse);
-    bot.postMessage(channel, question + realResponse);
+    else {
+      pollTester = 1;
+      var splittedText = args.split("|");
+      var question = splittedText[0];
+      var responseTemp = "";
+
+      for (i = 1; i < splittedText.length; i++) {
+        responseTemp += "|" + i + "." + splittedText[i];
+        number[i - 1] = 0;
+        answerCounter++;
+        answers[i] = splittedText[i];
+      }
+      var responses = responseTemp.split("|").join("\n");
+      bot.postMessage(channel, user.name + " started a poll! Vote with '!vote number' \n" + question + responses);
+      var pollInterval = setInterval(function () {
+        if(pollInterval===1) {
+          var message = "";
+          for (i = 1; i < splittedText.length; i++) {
+            message += "|" + i + "." + splittedText[i] + " ==> (" + number[i - 1] + ") votes";
+          }
+          var responses = message.split("|").join("\n");
+          bot.postMessage(channel, question + responses);
+        }
+        else{
+          stopInterval(pollInterval);
+        }
+      }, 120000);
+      setTimeout(function(){
+        for (i = 1; i < splittedText.length; i++) {
+          message += "|" + i + "." + splittedText[i] + " ==> (" + number[i-1] + ") votes";
+        }
+        var responses = message.split("|").join("\n");
+        bot.postMessage(channel, "Final results: \n" + question + responses);
+        number = [];
+        answerCounter = 0;
+        userVote = [];
+        pollTester = 0;
+        answers = [];
+      }, 960000);
+    }
+
   };
+
+  this.commands.vote = function(channel, args, user) {
+    var vote = args;
+
+    if (pollTester != 1){
+      bot.postMessage(channel, "No poll in progress");
+    }
+    else if(userVote.indexOf(user.name) != -1){
+      bot.postMessage(channel, user.name+ " already voted");
+    }
+    else{
+      userVote += user.name;
+      number[vote - 1]++;
+      bot.postMessage(channel, user.name+" voted");
+    }
+
+  };
+
+  this.commands.endpoll = function(channel, args, user) {
+    var message = "";
+    for (i = 1; i < answers.length; i++) {
+      message += "|" + i + "." + answers[i] + " ==> (" + number[i-1] + ") votes";
+    }
+    var responses = message.split("|").join("\n");
+    bot.postMessage(channel, "Final results: \n"  + responses);
+    number = [];
+    answerCounter = 0;
+    userVote = [];
+    pollTester = 0;
+    answers = [];
+  };
+
+  function stopInterval(int) {
+    clearInterval(int);
+  }
 
 };
 
